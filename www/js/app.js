460495d2ac18516bbf78d3634994c263fd813864
Oskar Löfgren <islander.abroad@gmail.com>
2014-12-12 23:14:17 +0100

    Applied different code style

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,21 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
-angular.module('iArenan', ['ionic', 'iArenan.controllers'])
+var app = angular.module('iArenan', ['ionic', 'iArenan.controllers'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
         }
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
+            StatusBar.styleDefault()
         }
-    });
+    })
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
 
     .state('app', {
@@ -23,8 +23,8 @@ angular.module('iArenan', ['ionic', 'iArenan.controllers'])
         abstract: true,
         templateUrl: "templates/menu.html",
         controller: 'AppCtrl'
-    });
+    })
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app');
-});
+    $urlRouterProvider.otherwise('/app')
+})
